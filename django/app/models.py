7e6aafd5522fc93555dc78c7476d7865a0c53517
Rohith K A <rohith.ka@epixelsolutions.com>
2022-10-18 11:33:01 +0530

    a

--- django/app/models.py
+++ django/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class slotModel(models.Model):
+    catagory = models.
+    location = 
+    creator =
+    nos =                                                                           #nos = number of slots
+    avs =                                                                            #available slots
+    time =
+    date = models.DateTimeField
+    status = models.BooleanField(default=False)
