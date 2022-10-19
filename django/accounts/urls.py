58f6f33f298575d199bdf3f9c5a35f31388b3983
Vaishnav E <vaishnave@epixelsolutions.com>
2022-10-19 14:01:03 +0530

    PrePull

--- django/accounts/urls.py
+++ django/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import path
 from django.conf.urls import url
-from .views import LoginPage ,Signup
+from .views import LoginPage ,Signup, SignupTurf
 from django.contrib.auth import views as auth_views
 
 # Create your views here.
@@ -9,6 +9,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', Signup.as_view(), name="register"),
+    path('register_turf/', SignupTurf.as_view(), name="register_turf"),
     path('', LoginPage.as_view(redirect_authenticated_user=True), name="login"),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 
--- django/e_commerce/views.py
+++ django/e_commerce/views.py
@@ -1,7 +1,16 @@
-from webbrowser import get
+from tkinter.tix import Form
 from django.shortcuts import render
 from django.views.generic import View
+from .forms import addStockForm
+from .models import ProductsModel
+from django.conf import settings  
+
 
 class E_commercePage(View):
     def get(self, request ,*args, **kwargs):
-        return render(request, 'e_commerce/shop.html',{ })
+        ecom_data = ProductsModel.objects.all()
+        print("heloooooooooooooooooo",ecom_data)
+        return render(request,'e_commerce/shop.html',{"ecom_data":ecom_data, 'media_url':settings.MEDIA_URL})
+
+
+        
