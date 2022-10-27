8b4c539cef11b620c2b97242fe2bfd990a248c0f
ASWIN KC <aswin.c@epixelsolutions.com>
2022-10-27 19:01:25 +0530

    create checkout page and add a requirement for math calculation

--- django/e_commerce/urls.py
+++ django/e_commerce/urls.py
@@ -1,9 +1,9 @@
 from django.urls import URLPattern, path
-from .views import  DeleteStock, E_commercePage
+from .views import  Checkout, DeleteStock, E_commercePage
 from dashboard.views import AddStockView
 
 urlpatterns = [
     path('', E_commercePage.as_view(), name="shop"),
     path('deletestock/<int:id>/',DeleteStock.as_view(),name='deletestock'),
-    # path('addstock/', AddStockView.as_view(), name="addstock"),
+    path('checkout/', Checkout.as_view(), name="checkout"),
 ]
--- django/e_commerce/views.py
+++ django/e_commerce/views.py
@@ -1,3 +1,4 @@
+from select import select
 from django.shortcuts import render,redirect
 from django.urls import reverse
 from django.views.generic import View
@@ -6,8 +7,16 @@ from .models import CartModel, ProductsModel
 from django.conf import settings 
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+import random
 
+from django import template
 
+register = template.Library()
+
+@register.simple_tag()
+def multiply(qty, unit_price, *args, **kwargs):
+    # you would need to do any localization of the result here
+    return qty * unit_price
 
 class E_commercePage(View):
     def get(self, request ,*args, **kwargs):
@@ -19,12 +28,19 @@ class E_commercePage(View):
         # for i in cartorbuy:
         #     cartorbuylist = cartorbuylist + list(i)
 
+        totalPrice = CartModel.objects.filter(username = request.user.username).values_list('quantity','price')
+        totalAmount = 0
+        totalItemCount = CartModel.objects.filter(username = request.user.username).count()
+        for i in totalPrice:
+            totalAmount = totalAmount + (i[0]*i[1])
         context = {
             'ecom_data':ecom_data, 
             'media_url':settings.MEDIA_URL,
             'cartForm':cartForm,
             'cartData':cartData,
             # 'cartorbuylist':cartorbuylist,
+            'totalAmount':totalAmount,
+            'totalItemCount':totalItemCount
         }
         helo="hello"
 
@@ -49,3 +65,29 @@ class DeleteStock(View):
         item = CartModel.objects.get(id=id)
         item.delete()
         return HttpResponseRedirect(reverse('shop'))
+
+class Checkout(View):
+    def get(self , request, *args, **kwargs):
+
+
+        num = int(random.random()*99999)
+        cartData = CartModel.objects.filter(username = request.user.username)
+        totalPrice = CartModel.objects.filter(username = request.user.username).values_list('quantity','price')
+        totalAmount = 0
+        totalItemCount = CartModel.objects.filter(username = request.user.username).count()
+        for i in totalPrice:
+            
+            totalAmount = totalAmount + (i[0]*i[1])
+
+
+        context = {
+            
+            'totalAmount':totalAmount,
+            'totalItemCount':totalItemCount,
+            'cartData':cartData,
+            'num':num,
+            
+        }
+
+
+        return render(request,'e_commerce/checkout.html',context)
