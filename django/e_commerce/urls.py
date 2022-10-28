7f9034195e16a573b63d1cc58c4e87fb8cdaa559
ASWIN KC <aswin.c@epixelsolutions.com>
2022-10-28 17:16:03 +0530

    checkout and admin product management

--- django/e_commerce/urls.py
+++ django/e_commerce/urls.py
@@ -1,9 +1,14 @@
 from django.urls import URLPattern, path
-from .views import  Checkout, DeleteStock, E_commercePage
+from .views import  Checkout,DeleteCartItem, DeleteStock, E_commercePage, OrderView, OutOfStock, StockTable
 from dashboard.views import AddStockView
 
 urlpatterns = [
     path('', E_commercePage.as_view(), name="shop"),
-    path('deletestock/<int:id>/',DeleteStock.as_view(),name='deletestock'),
+    path('deletecart/<int:id>/',DeleteCartItem.as_view(),name='deletecart'),
     path('checkout/', Checkout.as_view(), name="checkout"),
+    path('order/<int:id>/', OrderView.as_view(), name="order"),
+    path('outofstock/', OutOfStock.as_view(), name="outOfStock"),
+    path('stockdetails/', StockTable.as_view(), name="stocktable"),
+    path('deletestock/<int:id>/',DeleteStock.as_view(),name='deletestock'),
+     path('deletestock/<int:id>/',DeleteStock.as_view(),name='deletestock'),
 ]
