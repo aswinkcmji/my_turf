95396b6779b16a371e18c3583f14826d59c7df0b
Vaishnav E <vaishnave@epixelsolutions.com>
2022-11-18 17:39:45 +0530

    Changed username in usermodel to foriegn key of usermodel,displayed creator details,created a view for listing the players in a match,tested sending mail from app email,added dasboard url in navbar_boxed_fancy

--- django/static/assets/js/hope-ui.js
+++ django/static/assets/js/hope-ui.js
@@ -345,8 +345,8 @@ const loaderInit = () => {
     loader.classList.add('animate__animated', 'animate__fadeOut')
     setTimeout(() => {
       loader.classList.add('d-none')
-    }, 100)
-  }, 100)
+    }, 1000)
+  }, 1000)
 }
 
 /*---------------------------------------------------------------------
